4c666aeb2034f70268507f21df4d38e77adf4a3c
Ivanx95 <jaisen.95@gmail.com>
2024-02-14 16:41:03 -0500

    Add files via upload
    Fancy animations

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,93 @@
 let trucksSticky  = document.getElementsByClassName('sticky')[0];
 
-let trucksIncon  = document.getElementsByTagName('svg')[0];
-let  origOffsetY = trucksSticky.offsetTop;
+let trucksIncon1  = document.getElementsByTagName('svg')[0];
+let trucksIncon2  = document.getElementsByTagName('svg')[1];
+let trucksIncon3  = document.getElementsByTagName('svg')[2];
+// let  origOffsetY = trucksSticky.offsetTop;
 
+let allTrucks = [trucksIncon1,trucksIncon2,trucksIncon3];
 function convertRemToPixels(rem) {    
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
+function pixelToRem(px) {
+	return (px / (parseFloat(getComputedStyle(document.documentElement).fontSize))/2);
+}
 
-addEventListener('scroll',function onScroll(e) {
+function isElementInViewport (el) {
 
-	if(window.scrollY+convertRemToPixels(80)>=origOffsetY){
-		  trucksIncon.style.fill=("blue");
-		}else{
-			
+
+
+	var rect = el.getBoundingClientRect();
+
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+	);
+}
+
+
+
+function onVisibilityChange(el, callback) {
+	var old_visible;
+	return function () {
+		var visible = isElementInViewport(el);
+
+		if (visible != old_visible) {
+
+			if(visible){
+				let allExceptelement = [...allTrucks];
+
+				if(allExceptelement.includes(el)){
+					var index = allExceptelement.indexOf(el);
+					if (index !== -1) {
+						allExceptelement.splice(index, 1);
+					}
+				}
+				allExceptelement.forEach(e=>	{
+					e.style.fill=("red")
+					e.classList.remove("animate-shake");
+				});
+				if (typeof callback == 'function') {
+					callback();
+				}
+			}
+			old_visible = visible;
 		}
+	}
+}
 
-});
+let handlers = allTrucks.map(e=> onVisibilityChange(e, ()=>{
+	e.style.fill=("blue");
+	e.classList.add("animate-shake");
+
+	e.addEventListener("animationend", () => {
+		console.log(e.parentElement.style.top);
+		e.parentElement.style.top =e.parentElement.style.top;
+	});
+
+}));
+
+
+
+if (window.addEventListener) {
+	handlers.forEach(f=>{
+		addEventListener('DOMContentLoaded', f, false);
+		addEventListener('load', f, false);
+		addEventListener('scroll', f, false);
+		addEventListener('resize', f, false);
+	})
+
+}
+
+// addEventListener('scroll',function onScroll(e) {
+//
+// 	if(window.scrollY+convertRemToPixels(80)>=origOffsetY){
+// 		  trucksIncon.style.fill=("blue");
+// 		}else{
+//
+// 		}
+//
+// });
