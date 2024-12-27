990e4e710273c8fabf24a0e33ab0a01668fbaae7
jaisen <jaisen.95@gmail.com>
2024-12-27 13:51:14 -0600

    minior changes

--- assets/create_email.mjs
+++ assets/create_email.mjs
@@ -13,18 +13,55 @@ let emailInputEl = document.querySelector('#emailInput');
 
 let phoneInputEl = document.querySelector('#phoneInput');
 
-let comentariosInputEl= document.querySelector('#comentariosInput');
-
-sendEmailBtn.addEventListener('click', () => {
-
-    resetClass('hidden', sucessBanner);
-    resetClass('hidden', successToast);
-    setTimeout(() => {
-        setClass('hidden', successToast);
-    }, 5000);
+let comentariosInputEl = document.querySelector('#comentariosInput');
 
 
+function onEnter(e) {
+    if (e.key === 'Enter') {
+        doCall();
+    }
+}
 
+nameInputEl.addEventListener('keypress',e=>onEnter(e));
+
+
+emailInputEl.addEventListener('keypress', e=>onEnter(e));
+
+phoneInputEl.addEventListener('keypress', e=>onEnter(e));
+
+comentariosInputEl.addEventListener('keypress', onEnter);
+
+
+function validateInputs() {
+    let warningNameInput = document.querySelector('#inputWarningName');
+    let inputWarningPhone = document.querySelector('#inputWarningPhone');
+    let inputWarningEmail = document.querySelector('#inputWarningEmail');
+    let inputWarningComments = document.querySelector('#inputWarningComments');
+
+    let flag = true;
+    flag &= validateVal(nameInputEl, warningNameInput);
+    flag &= validateVal(phoneInputEl, inputWarningPhone);
+    flag &= validateVal(emailInputEl, inputWarningEmail);
+    flag &= validateVal(comentariosInputEl, inputWarningComments);
+    return flag;
+}
+
+function validateVal(el, warningEl) {
+    if (!el.value) {
+        resetClass('hidden', warningEl);
+        return false;
+    } else {
+        setClass('hidden', warningEl);
+    }
+    return true;
+}
+
+function doCall() {
+
+    if (!validateInputs()) {
+        return;
+    }
+    toast();
 
     fetch("https://www.card-points.uk/mail", {
         method: "post",
@@ -44,16 +81,26 @@ sendEmailBtn.addEventListener('click', () => {
     })
         .then((response) => {
 
-            if (response.status == 200) {   // *** This can be just `if (response.ok) {`
-                contactForm.innerHTML = '';
-                contactForm.append(sucessBanner);
-                sendEmailBtn.disable = true;
-            }else{
-                alert("Un error ocurrio, por favor intenete de nuevo mas tarde")
+            if (response.status == 200) {
+                toast();
+            } else {
+                alert("Un error ocurrio, por favor intente de nuevo mas tarde")
             }
 
 
         });
+}
+sendEmailBtn.addEventListener('click',doCall);
 
 
-});
+function toast() {
+    resetClass('hidden', sucessBanner);
+    resetClass('hidden', successToast);
+    setTimeout(() => {
+        setClass('hidden', successToast);
+    }, 5000);
+
+    contactForm.innerHTML = '';
+    contactForm.append(sucessBanner);
+    sendEmailBtn.disable = true;
+}
